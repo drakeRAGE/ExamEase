6c02bdae9c54b5824d82adba2f3235a4eac8bf13
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2025-08-07 18:13:36 +0530

    feat(client): initialize React app with Vite and Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='h-screen bg-gradient-to-r from-pink-200 to-purple-400 flex items-center justify-center text-4xl text-purple-600 font-serif font-bold'>Hlo, I am Drag!!</h1>
+    </>
+  )
+}
+
+export default App
